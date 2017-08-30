0924cbc8c86ebc451d4348c61ceee69eb9f613ba
bgee0915 <bgee0915@outlook.com>
2017-08-30 17:24:09 +0800

    泛型的知识点补充

--- src/com/bgee/base/generic/GenericDemo1.java
+++ src/com/bgee/base/generic/GenericDemo1.java
@@ -1,6 +1,8 @@
 package com.bgee.base.generic;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 1： 	为泛型加上限定 ,  T 必须为 comparable(根据你的限定类而变化) 的子类型 
@@ -12,7 +14,12 @@ import java.io.Serializable;
  * 7：	泛型使用要考虑许多的限制, 大多都是由类型擦除引起的  (第五条)
  * 8： 	禁止使用带有类型变量的静态域方法
  * 9：	不能抛出或使用泛型类的实例
+ * 10：	上边界  <? extends Fruit>	必须为 Fruit 的子类型
+ * 11:	下边界 <? super Banana>	必须为 Banana 的父类型
  * 
+ * 12:  如果要从集合中读取类型T的数据，并且不能写入，可以使用 ? extends 通配符；(Producer Extends)
+		如果要从集合中写入类型T的数据，并且不需要读取，可以使用 ? super 通配符；(Consumer Super)
+		如果既要存又要取，那么就不要使用任何通配符。
  *  
  *  
  * @author bgee0915
@@ -21,4 +28,37 @@ import java.io.Serializable;
  */
 public class GenericDemo1<T extends Comparable<T> & Serializable> {
 
+	public static void main(String[] args) {
+		
+//		ok
+		List<Object> list = new ArrayList<>();
+		list.add(new Fruit());
+		
+//		上界
+		List<? extends Fruit> upList = new ArrayList<>();
+		upList.add(null);  //ok
+//		upList.add(new Fruit());  	// boom!
+//		upList.add(new Apple());	// boom!
+//		upList.add(new Banana());	// boom!
+		Fruit fruit = upList.get(0);
+		
+//		下界
+		List<? super Banana> downList = new ArrayList<>();
+		downList.add(new RedBanana());
+		downList.add(new Banana());
+//		downList.add(new Fruit()); 			// boom!
+		
+//		Banana banana = downList.get(0);	// boom!
+	}
+	
 }
+
+
+class Fruit{}
+
+class Apple extends Fruit{}
+
+class Banana extends Fruit{}
+
+class RedBanana extends Banana{}
+
