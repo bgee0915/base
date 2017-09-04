032492b79718be9c94d5452d925d6a381a3ed17a
bgee0915 <bgee0915@outlook.com>
2017-09-04 22:32:04 +0800

    关于反射

---
+++ src/com/bgee/base/reflect/ReflectDemo1.java
@@ -0,0 +1,29 @@
+package com.bgee.base.reflect;
+
+
+import com.bgee.base.util.FunUtil;
+
+/**
+ * 反射实例1
+ * @author bgee0915
+ *
+ */
+public class ReflectDemo1 {
+	public static void main(String[] args) {
+		Demo1 demo1 = new Demo1();
+		Class<?> demo1Class = demo1.getClass();
+		
+//		类的全路径名称
+		String absoluteName = demo1Class.getName();
+		
+//		类名
+		String className = demo1Class.getSimpleName();
+		
+		FunUtil.println(absoluteName,className);
+		
+	}
+}
+
+class Demo1{
+	
+}
---
+++ src/com/bgee/base/reflect/ReflectDemo2.java
@@ -0,0 +1,5 @@
+package com.bgee.base.reflect;
+
+public class ReflectDemo2 {
+
+}
