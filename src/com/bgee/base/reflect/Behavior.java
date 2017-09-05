940984666f55aefeb0ab2fef5a8b35af2cd68fb8
bgee0915 <bgee0915@outlook.com>
2017-09-05 18:14:18 +0800

    反射 或许类的属性

--- src/com/bgee/base/reflect/Behavior.java
+++ src/com/bgee/base/reflect/Behavior.java
@@ -2,6 +2,7 @@ package com.bgee.base.reflect;
 
 
 public  interface Behavior {
+	public static final String fieldOne = "1";
 	void sleep();
 	void eat();
 }
@@ -10,34 +11,57 @@ public  interface Behavior {
 abstract class Animal{
 	String name;
 	int age;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	@Override
-	public String toString() {
-		return "Animal [name=" + name + ", age=" + age + "]";
-	}
+	public String fatherField;
+	
+	abstract void fuck();
 }
 
 class Dog extends Animal implements Behavior {
-
+	
 	@Override
 	public void sleep() {
 		
 	}
-
+ 
 	@Override
 	public void eat() {
 		
 	}
+	
+	@Override
+	public void fuck() {
+		
+	}
+
+	
+	
+	
+	public int thisField;
+	private int age;
+	private String name;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Dog [age=" + age + ", name=" + name + "]";
+	}
+
+	
+	
 	
 }
---
+++ src/com/bgee/base/reflect/ReflectDemo9.java
@@ -0,0 +1,44 @@
+package com.bgee.base.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import com.bgee.base.util.FunUtil;
+
+public class ReflectDemo9 {
+	public static void main(String[] args) {
+		Class<?> classdemo9 = null;
+		try {
+			classdemo9 = Class.forName("com.bgee.base.reflect.Dog");
+			
+//			属性 数组 // 该类的所有字段
+			Field []f = classdemo9.getDeclaredFields();
+			
+			for(int i=0; i<f.length; i++) {
+				
+//				权限修饰符
+				int modifierNum = f[i].getModifiers();
+				String modifier = Modifier.toString(modifierNum);
+				
+//				属性类型
+				String typeName = f[i].getType().getName();
+				
+				FunUtil.print(modifier," ", typeName, " " , f[i].getName() , "\n");
+			}
+			
+			
+			
+			//所有的    自己的 或者 继承的父类 或者 实现的接口的 public 的 字段
+			Field []superF = classdemo9.getFields();
+			System.out.println(superF.length);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+}
