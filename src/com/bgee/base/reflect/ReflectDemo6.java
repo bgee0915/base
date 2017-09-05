5220ce1f8b29cab8cea0574fb98460c8e4f591b1
bgee0915 <bgee0915@outlook.com>
2017-09-05 09:39:40 +0800

    通过反射获知继承的父类

---
+++ src/com/bgee/base/reflect/ReflectDemo6.java
@@ -0,0 +1,28 @@
+package com.bgee.base.reflect;
+
+import com.bgee.base.util.FunUtil;
+
+/**
+ * 通过反射获知 继承的父类
+ * @author bgee0915
+ *
+ */
+public class ReflectDemo6 {
+	public static void main(String[] args) {
+		
+		Class<?> classdemo6 = null;
+		try {
+			
+			classdemo6 = Class.forName("com.bgee.base.reflect.Dog");
+			Class<?> superClass = classdemo6.getSuperclass();
+			FunUtil.println(superClass.getName());
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+}
