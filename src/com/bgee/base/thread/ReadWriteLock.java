b18c069461bb9f16c65e6beebe857d3c090aa153
bgee0915 <bgee0915@outlook.com>
2017-09-11 18:11:57 +0800

    读写所 读取优化

--- src/com/bgee/base/thread/ReadWriteLock.java
+++ src/com/bgee/base/thread/ReadWriteLock.java
@@ -1,39 +1,81 @@
 package com.bgee.base.thread;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 读写锁
  * @author bgee0915
  *
  */
 public class ReadWriteLock {
-	private int reader = 0;
+	
+	private Map<Thread,Integer> readingThreads = new HashMap<Thread,Integer>();
 	private int writer = 0;
 	private int requestWrite = 0;
 
 	public synchronized void lockRead() throws InterruptedException {
-		if(writer > 0 || requestWrite > 0) {
+		Thread callingThread = Thread.currentThread();
+		while(!canGrantReadAccess(callingThread)) {
 			wait();
 		}
-		reader++;
+		readingThreads.put(callingThread, getReadAccessCount(callingThread) + 1);
 	}
 	
 	public synchronized void unlockRead() {
-		reader--;
+		Thread callingThread = Thread.currentThread();
+		int num = getReadAccessCount(callingThread);
+		if(1 == num) {
+			readingThreads.remove(callingThread);
+			return;
+		}
+		
+		readingThreads.put(callingThread,num + 1);
 		notifyAll();
 	}
 	
 	public synchronized void lockWrite() throws InterruptedException {
-		requestWrite++;
-		if(reader > 0 || writer > 0) {
-			wait();
-		}
-		requestWrite--;
-		writer++;
+//		requestWrite++;
+//		if(reader > 0 || writer > 0) {
+//			wait();
+//		}
+//		requestWrite--;
+//		writer++;
 		
 	}
 	
 	public synchronized void unlockWrite() {
 		writer--;
 		notifyAll();
+	}
+	
+	
+	
+	private int getReadAccessCount(Thread callingThreads) {
+		Integer num = readingThreads.get(callingThreads);
+		if(null == num) {
+			return 0;
+		}
+		return num.intValue();
+	}
+	
+	private boolean canGrantReadAccess(Thread callingThreads) {
+		if(writer > 0) {
+			return false;
+		}
+		
+		if(requestWrite > 0) {
+			return false;
+		}
+		
+		if(!isReader(callingThreads)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private boolean isReader(Thread callingThread) {
+		return readingThreads.get(callingThread) != null;
 	}
 }
