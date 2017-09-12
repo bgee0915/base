2437ed5d428223723fc49ed869f0b88e46327ce8
bgee0915 <bgee0915@outlook.com>
2017-09-12 22:17:11 +0800

    UPDATE

--- src/com/bgee/base/thread/ThreadDemo2.java
+++ src/com/bgee/base/thread/ThreadDemo2.java
@@ -5,37 +5,38 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadDemo2 implements Runnable{
 	String name;
-	private static int num = 0;
+	private Calc calc = new Calc();
 	Lock lock = new ReentrantLock();
-	
-
 	
 	public static void main(String[] args) {
 
-		int j = 0;
-		while(j++ < 5) {
-			new Thread(new ThreadDemo2(j+"")).start();
-		}		
+		for(int i=1; i<=10; i++) {
+			new Thread(new ThreadDemo2(i+"")).start();
+		}
 		
 	}
 
 	@Override
 	public void run() {	
-		for(int i=0; i<20; i++) {
-			numAdd();
+		for(int i=0; i<10; i++) {
+			calc.numAdd();
 		}
+		System.out.println("thread-" + Thread.currentThread().getName() + "    ---  num=" + Calc.num);
 	}
 	
-	public void numAdd() {
-		lock.lock();
-			++num;
-			System.out.println("thread" + this.name + " -  num:  " + num);
-		lock.unlock();
-	}
+	
 	
 	ThreadDemo2(String name){
 		this.name = name;
 	}
+}
+
+class Calc{
+	static int num = 0;
+	public void numAdd() {
+		num+=2;
+	}
+	
 }
 
 
